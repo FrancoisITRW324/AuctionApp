ef6f4ffd4d923b72d6f8407eca60eef96dc06026
Francois 27139433 <27139433@student.g.nwu.ac.za>
2020-01-12 21:44:31 +0200

    login and register in angular

--- AuctionApp-SPA/src/app/app.module.ts
+++ AuctionApp-SPA/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { appRoutes } from './routes';
+import { AuthService } from './_services/auth.service';
+
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent,
+      LoginComponent,
+      RegisterComponent,
+      HomeComponent,
+      AboutComponent,
+      ContactComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule,
+      RouterModule.forRoot(appRoutes)
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
